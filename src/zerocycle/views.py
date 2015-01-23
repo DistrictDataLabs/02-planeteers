61b5332922e8fe8c5394f53fc5e80c10fcf89034
EyuelAbebe <eyueld@gmail.com>
2015-01-23 02:33:36 -0500

    Added corresponding view functions for the index, charts, tables and forms links.

--- src/zerocycle/views.py
+++ src/zerocycle/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render, render_to_response, RequestContext
 
-def home(reqest):
+def home(request):
 
   return render_to_response("index.html",
                             locals(),
-                            context_instance=RequestContext(request))
+                            context_instance= RequestContext(request))
+
+def charts(request):
+
+  return render_to_response("charts.html",
+                            locals(),
+                            context_instance= RequestContext(request))
+
+
+def tables(request):
+
+  return render_to_response("tables.html",
+                            locals(),
+                            context_instance= RequestContext(request))
+
+
+def forms(request):
+
+  return render_to_response("forms.html",
+                            locals(),
+                            context_instance= RequestContext(request))
+
